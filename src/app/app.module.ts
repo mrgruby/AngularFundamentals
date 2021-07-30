23297816ae06c80edc4d3329c2383b25c993d7e2
Søren Gruby <sogru@dtu.dk>
2021-07-30 15:36:53 +0200

    Module 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { EventsListComponent } from './events/events-list/events-list.component';
+import { NavbarComponent } from './nav/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsListComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
